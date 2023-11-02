39a8b1c2eb6e00c2db5b9b3e2fe87b38ef61cd82
nayoong02 <sonyangel02@naver.com>
2023-11-02 18:30:50 +0900

    #41 - 모델 추론 후 수동 고장 예측, 실시간 고장 예측 로직 처리

--- SDSL/lib/pages/monitoring.dart
+++ SDSL/lib/pages/monitoring.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sdsl/components/customAppbar.dart';
+import 'dart:async';
 
 class Monitoring extends StatefulWidget {
   @override
@@ -10,25 +11,73 @@ class _MonitoringState extends State<Monitoring> {
   // 각각의 고장 코드와 내용
   List<String> faultCodes = ['P0035', 'P0122', 'P0135', 'P0339', 'P0562'];
   List<String> faultDescriptions = [
-    '터보차저나 슈퍼차저 바이패스 밸브에\n이상이 생겼어요.',
-    '스로틀 위치 센서의 전압이 너무 낮아요.\n',
-    'O2 산소 센서 히터 회로가 오작동하고\n있어요.',
-    '크랭크축 위치 센서 회로가 오작동하고 있어요.',
-    '시스템 전압이 너무 낮아요.\n',
+    '터보 엔진에서 연료와 공기의 비율을\n감지하는 산소 센서의 문제가 생겼어요.',
+    '엔진의 공기 공급을 제어하는 부분에\n문제가 발생했어요.',
+    '일반 엔진에서 연료와 공기의 비율을\n감지하는 센서의 문제가 발생했어요.',
+    '엔진이 언제 회전하고 멈추는지\n파악하는 센서의 문제가 발생했어요.',
+    '배터리가 충분히 충전되지 않거나\n배터리 내부적으로 문제가 발생했어요.',
   ];
 
   // 각 고장 코드의 알림 횟수를 저장하는 변수
-  List<int> notificationCount = [5, 15, 30, 4, 52];
+  List<int> notificationCount = [0, 0, 0, 0, 0];
+  Timer? timer;
 
   // 알림 횟수에 따라 색상을 반환하는 함수
   Color getContainerColor(int count) {
-    if (count >= 50) {
-      return Color(0xbdef0000); // count가 50 이상이면 빨간색
-    } else if (count > 10) {
-      return Color(0x64ff1703); // count가 10 초과 50 미만이면 주황색
-    } else {
-      return Color(0x27ff3737); // 나머지 경우에는 다른 색
+    if (count >= 15) { // count가 15 이상이면
+      return Color(0xffff0025);
+    } else if (count >= 10) { // count가 10 이상이면
+      return Color(0xffff6e2e);
+    } else if (count >= 5) { // count가 5 이상이면
+      return Color(0xFFFFA74F);
+    } else { // 나머지
+      return Color(0xFFFFC637);
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _increaseArrayValues(); // 배열 카운트 증가
+  }
+
+  @override
+  void dispose() {
+    _stopArrayIncrease();
+    super.dispose();
+  }
+
+  void _increaseArrayValues() {
+    // 5초에 한 번씩 배열 값 증가
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        notificationCount[0]++;
+      });
+    });
+    timer = Timer.periodic(Duration(seconds: 2), (timer) {
+      setState(() {
+        notificationCount[1]++;
+      });
+    });
+    timer = Timer.periodic(Duration(seconds: 3), (timer) {
+      setState(() {
+        notificationCount[2]++;
+      });
+    });
+    timer = Timer.periodic(Duration(seconds: 4), (timer) {
+      setState(() {
+        notificationCount[3]++;
+      });
+    });
+    timer = Timer.periodic(Duration(seconds: 5), (timer) {
+      setState(() {
+        notificationCount[4]++;
+      });
+    });
+  }
+
+  void _stopArrayIncrease() {
+    timer?.cancel();
   }
 
   @override
@@ -48,7 +97,7 @@ class _MonitoringState extends State<Monitoring> {
           children: <Widget>[
             SizedBox(height: 20),
             Text(
-              '실시간 자동예측 내역',
+              '실시간 예측 내역',
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 25,
--- SDSL/lib/pages/setting.dart
+++ SDSL/lib/pages/setting.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
+//import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Setting extends StatefulWidget {
   @override
@@ -7,7 +9,8 @@ class Setting extends StatefulWidget {
 }
 
 class _SettingState extends State<Setting> {
-  bool notificationsEnabled = true; // 초기 상태: 알림 활성화
+  bool notificationEnabled = false; // 초기 상태 : 알림 비활성화
+  List<int> countList = [0, 0, 0, 0, 0];
 
   // 정보 다이얼로그를 표시하는 함수
   void _showInfoDialog() {
@@ -15,8 +18,8 @@ class _SettingState extends State<Setting> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('정보'),
-          content: Text('백그라운드 실행을 활성화하면 예측 경고 알림을 실시간으로 확인해볼 수 있습니다.'),
+          title: Text('주의사항'),
+          content: Text(
               '- 배터리 수명이 감소할 수 있어요.\n- 다른 앱의 실행이 느려질 수 있어요.\n- 발열 현상이 나타날 수 있어요.'),
           actions: <Widget>[
             TextButton(
@@ -31,29 +34,60 @@ class _SettingState extends State<Setting> {
     );
   }
 
+  @override
+  void initState() {
+    super.initState();
+
+    // 앱을 시작할 때 이전 설정 불러오기
+    _loadSettings();
+  }
+
+  void _loadSettings() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? savedNotificationEnabled = prefs.getBool("notificationEnabled");
+    if (savedNotificationEnabled != null) {
+      setState(() {
+        // 앱을 처음 실행하는 경우에만 설정 불러오기
+        notificationEnabled = savedNotificationEnabled;
+      });
+    }
+  }
+
+  Timer? timer;
+
   // 스위치 상태에 따라 푸시 알림을 활성화 또는 비활성화
   void _toggleNotifications(bool value) async {
     setState(() {
-      notificationsEnabled = value;
+      notificationEnabled = value;
+      print(value);
+
+      // 스위치 상태가 변경될 때마다 설정을 저장
+      _saveSettings(value);
     });
 
-    final FirebaseMessaging messaging = FirebaseMessaging.instance;
+    // 알림 관련 기능 일단 제외
+    // final FirebaseMessaging messaging = FirebaseMessaging.instance;
+    //
+    // if (value) {
+    //   try {
+    //     String? token = await messaging.getToken();
+    //     print("토큰 : $token");
+    //   } catch (e) {
+    //     print("토큰을 가져오는 중 오류 발생: $e");
+    //   }
+    // } else {
+    //   try {
+    //     await messaging.deleteToken();
+    //     print("토큰을 삭제했습니다.");
+    //   } catch (e) {
+    //     print("토큰을 삭제하는 중 오류 발생: $e");
+    //   }
+    // }
+  }
 
-    if (value) {
-      try {
-        String? token = await messaging.getToken();
-        print("토큰 : $token");
-      } catch (e) {
-        print("토큰을 가져오는 중 오류 발생: $e");
-      }
-    } else {
-      try {
-        await messaging.deleteToken();
-        print("토큰을 삭제했습니다.");
-      } catch (e) {
-        print("토큰을 삭제하는 중 오류 발생: $e");
-      }
-    }
+  void _saveSettings(bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool("notificationEnabled", value);
   }
 
   @override
@@ -81,16 +115,19 @@ class _SettingState extends State<Setting> {
               children: <Widget>[
                 Row(
                   children: <Widget>[
-                    Text(
-                      '백그라운드 실행',
-                      style: TextStyle(
-                        color : Colors.black54,
-                        fontSize: 20.0,
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        '백그라운드 실행',
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 20.0,
+                        ),
                       ),
                     ),
                     IconButton(
-                      icon: Icon(Icons.info), // 정보 아이콘 사용
-                      color: Colors.black54,
+                      icon: Icon(Icons.info,
+                          color: Colors.blueAccent), // 정보 아이콘 사용
                       onPressed: () {
                         // 정보 다이얼로그 표시
                         _showInfoDialog();
@@ -99,7 +136,7 @@ class _SettingState extends State<Setting> {
                   ],
                 ),
                 Switch(
-                  value: notificationsEnabled,
+                  value: notificationEnabled,
                   onChanged: (value) {
                     // 스위치 상태 변경 시 호출되는 함수
                     _toggleNotifications(value);
@@ -116,7 +153,8 @@ class _SettingState extends State<Setting> {
           padding: EdgeInsets.all(16.0),
           child: ElevatedButton(
             onPressed: () {
-              Navigator.pop(context);
+              // home으로 토글 값 반환
+              Navigator.pop(context, notificationEnabled);
             },
             child: Text(
               '확인',
